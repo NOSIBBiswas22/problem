23d9b1a2f06e5b4ec0bf3c12e629b817f263cb28
Nosib Biswas <88088042+NOSIBBiswas22@users.noreply.github.com>
2025-10-18 12:31:53 +0600

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int n=8;
+    for(int i=0; i<n; i++){
+        // spaces
+        for(int j=1; j<n-i; j++){
+            printf(" ");
+        }
+        // stars
+        for(int j=1; j<=i+1; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    for(int i=0; i<n; i++){
+        // stars
+        for(int j=1; j<=i+1; j++){
+            printf(" ");
+        }
+        // spaces
+        for(int j=1; j<n-i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
